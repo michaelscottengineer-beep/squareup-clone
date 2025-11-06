053bd84a99472ba5e55ea4ced70d67faf12914a3
nvminh155 <minhnv155@gmail.com>
2025-11-06 14:29:53 +0700

    show img item ở order detail

--- src/pages/dashboard/orders/OrderDetail.tsx
+++ src/pages/dashboard/orders/OrderDetail.tsx
@@ -17,6 +17,7 @@ import type {
 import { formatDate } from "date-fns";
 import { Separator } from "@/components/ui/separator";
 import { Mail } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const OrderDetail = () => {
   const { orderId } = useParams();
@@ -67,18 +68,24 @@ const OrderDetail = () => {
         </span>
       </div>
 
-      <div className="order-items mt-8  border border-border rounded-lg p-4">
+      <div className="order-items mt-8  border border-border rounded-lg p-4 space-y-4">
         <h2 className="text-xl font-semibold">Order Items</h2>
         {convertFirebaseArrayData<TOrderCartItem>(order.cartItems).map(
-          (item) => {
+          (item, i) => {
             const totalItem = Number(item?.price ?? 1) * item.quantity;
             const totalModifier = item.modifier
               ? Number(item.modifier.price ?? 0) * item.quantity
               : 0;
 
             return (
-              <div key={item.id} className="flex items-center gap-4">
-                <div className="img w-8 bg-red-50 rounded-lg h-full aspect-square"></div>
+              <div key={item.id} className={cn("flex items-start gap-4 border-b border-border pb-2", {
+                "border-b-0": i === Object.keys(order?.cartItems ?? {}).length -1
+              })}>
+                {item.image ? (
+                  <img src={item.image} alt="img-item" className="w-8 h-8 rounded-md" />
+                ) : (
+                  <div className="img w-8 bg-red-50 rounded-lg h-full aspect-square"></div>
+                )}
                 <div className="grid grid-cols-[1fr_auto] text-sm gap-2 flex-1">
                   <div className="flex items-center gap-2">
                     <p className="text-gray-900 font-semibold">{item.name} </p>
@@ -100,7 +107,7 @@ const OrderDetail = () => {
                     ${totalModifier.toFixed(2)}
                   </p>
 
-                  <p className="col-span-2 font-semibold text-gray-900 text-end ">
+                  <p className="col-span-2 font-semibold text-gray-900 text-end mt-2">
                     <span className="border-t border-t-gray-900">Total: </span>
                     <span className="border-t border-t-gray-900">
                       {(totalModifier + totalItem).toFixed(2)}
@@ -113,10 +120,14 @@ const OrderDetail = () => {
         )}
       </div>
 
-      <div className="order-info  border border-border rounded-lg p-4" >
+      <div className="order-info  border border-border rounded-lg p-4">
         <h2 className="text-xl font-semibold mb-2">Order Info</h2>
-        {order.basicInfo.shippingMethod === "Delivery" && <OrderInfoDelivery order={order} />}
-        {order.basicInfo.shippingMethod === "Dine In" && <OrderInfoDineIn order={order} />}
+        {order.basicInfo.shippingMethod === "Delivery" && (
+          <OrderInfoDelivery order={order} />
+        )}
+        {order.basicInfo.shippingMethod === "Dine In" && (
+          <OrderInfoDineIn order={order} />
+        )}
       </div>
 
       {/* Order Summary */}
